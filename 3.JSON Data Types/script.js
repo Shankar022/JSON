c41dec4286e6af54ab164c3182a227d9602d251a
TonyTws <shankarweb22@gmail.com>
2021-06-09 17:19:24 +0530

    json data types

---
+++ 3.JSON Data Types/script.js
@@ -0,0 +1,25 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET','test.json');
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4){
+        if(xhr.status === 200){
+            let data = JSON.parse(xhr.responseText);
+            show(data);
+        }
+    }
+};
+xhr.send();
+show = (data) =>{
+    console.log(data)
+};
+
+/*
+JSON values cannot use any of the following data types:
+
+function
+
+date
+
+Undefined
+ */
